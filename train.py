ae93f5b703c2e5f46cd27755025e8e2f2c1b2b44
genkimats <is0743hf@ed.ritsumei.ac.jp>
2026-06-21 23:31:36 +0900

    small window size exp

--- train.py
+++ train.py
@@ -42,9 +42,9 @@ BASE_FEATURES = ["x-cord", "y-cord"]
 KINEMATIC_FEATURES = ["velocity", "acceleration", "jerk", "velocity-mag", "angular-vel"]
 
 # (Window variables are ignored if USE_FULL_LENGTH is True)
-WINDOW_SIZE = 512
+WINDOW_SIZE = 128
 
-OVERLAP = 200
+OVERLAP = 50
 
 NUM_VERTICES = 65
 TOLERANCE_WINDOW = 5       
